93df8729cc27c7a428c26e312d9b35d6279cc70e
varnavasudevan <varna.vasudevan@gmail.com>
2018-10-24 10:41:25 -0700

    take out image fade about

--- assets/js/about_me_script.js
+++ assets/js/about_me_script.js
@@ -4,10 +4,10 @@ $(window).load(function() {
 		$(".se-pre-con").fadeOut(600);;
 	});
 	
-$("#container").hover(function(){
-  		 $('.cover_img_scaled').addClass("cover_img_hover_jquery");
+// $("#container").hover(function(){
+//   		 $('.cover_img_scaled').addClass("cover_img_hover_jquery");
 
-    });
+//     });
 
 $(".arrowimage").click(function(){
   
