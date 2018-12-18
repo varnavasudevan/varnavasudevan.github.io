5e6b4704c795e46fc9f6962ead6fd568007e2ca2
varnavasudevan <varna.vasudevan@gmail.com>
2018-12-18 01:29:47 -0800

    rendering and social butterfly pages

---
+++ assets/js/script_images.js
@@ -0,0 +1,39 @@
+
+
+
+ $(window).on('load', function() { // makes sure the whole site is loaded 
+            $('#status').fadeOut(); // will first fade out the loading animation 
+            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+            $('body').delay(350).css({'overflow':'visible'});
+        })
+ 
+$(document).ready(function(){
+
+
+$("#tinkeringpage").click(function(){
+  
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href')).offset().top
+    }, 300);
+
+    
+
+    return false;
+
+    });
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+
+});
